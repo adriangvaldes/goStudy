6b30f631e50c06d54d0e79d84f289328664f28e0
Adrian Valdes <adriangvaldes@gmail.com>
2024-05-15 08:14:57 -0300

    Teste2

--- channels/main.go
+++ channels/main.go
@@ -33,7 +33,6 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		fmt.Println(link, "might be down!")
 		c <- link
 		return
 	}
